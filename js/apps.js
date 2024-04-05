4baef34eb61d344415eeb02757df5eb96bcbc121
78 mmd 69 <mmdqurani@gmail.com>
2024-04-05 17:22:38 +0330

    Fix : Debug program performance

--- js/apps.js
+++ js/apps.js
@@ -1,25 +1,31 @@
 let $ = document;
 
-let body = $.body;
 let boxText = $.getElementById ("box_text");
-let keyCode = $.getElementById ("key_code");
-let boxsEventKey = $.getElementById ("box_event_key");
-let boxsEventLocation = $.getElementById ("box_event_location");
-let boxsEventWhich = $.getElementById ("box_event_which");
-let boxsEventCode = $.getElementById ("box_event_code");
+let eventkeyCode = $.getElementById ("key_code");
+let eventKey = $.getElementById ("event_key");
+let eventLocation = $.getElementById ("event_location");
+let eventWhich = $.getElementById ("event_which");
+let eventCode = $.getElementById ("event_code");
+let allBox = $.getElementById ("box_event_obj");
 
 
-body.addEventListener ("keydown" , (event) => {
+document.body.addEventListener ("keydown" , (event) => {
     event.preventDefault();
 
+    boxText.style.display = "none";
+    eventkeyCode.style.display = "flex";
+    allBox.style.display = "flex";
+
     let saveValueCode = event.code;
+    let saveValueKeyCode = event.keyCode;
     let saveValueKey = event.key;
-    let saveValueWhich = event.keyCode;
+    let saveValueWhich = event.which;
     let saveValueLocation = event.location;
 
-    boxsEventKey.innerHTML = '<span id="text_value">'+saveValueKey+'</span>';
-    boxsEventLocation.innerHTML = '<span id="text_value">'+saveValueLocation+'</span>';
-    boxsEventWhich.innerHTML = '<span id="text_value">'+saveValueWhich+'</span>';
-    boxsEventCode.innerHTML = '<span id="text_value">'+saveValueCode+'</span>';
+    eventKey.innerHTML = saveValueKey;
+    eventkeyCode.innerHTML = saveValueKeyCode;
+    eventLocation.innerHTML = saveValueLocation;
+    eventWhich.innerHTML = saveValueWhich;
+    eventCode.innerHTML = saveValueCode;
 
 });
